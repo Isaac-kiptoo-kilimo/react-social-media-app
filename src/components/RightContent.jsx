825b6aa5b07afa44110b357583cec0322c670769
unknown <isaackilimok2@gmail.com.com>
2024-01-30 12:58:06 +0300

    feat:fixing width

--- src/components/RightContent.jsx
+++ src/components/RightContent.jsx
@@ -6,7 +6,9 @@ import vectoround from '../assets/vectoround.png'
 import star from '../assets/star.png'
 import PostImage from '../assets/post-image1.png'
 import PostImage2 from '../assets/post-image2.png'
-import Share from '../assets/share.png'
+import Like from '../assets/heart.png'
+import Smile from '../assets/mood-smile.png'
+import Link from '../assets/link.png'
 import Message from '../assets/message-circle.png'
 import Share2 from '../assets/share.png'
 
@@ -57,13 +59,26 @@ const RightContent = () => {
                 <img src={PostImage} alt="" />
                 <img src={PostImage2} alt="" />
             </div>
-            <div className="likes">
-                <img src={Share} alt="" />
+            <div className="like-comment">
+                <div className="likes">
+                <img src={Like} alt="" />
+                <p>2.6k <span>Likes</span></p>
+                </div>
+                <div className="comments">
                 <img src={Message} alt="" />
+                <p>297 <span>comments</span></p>
+                </div>
+                <div className="shares">
                 <img src={Share2} alt="" />
+                <p>201 <span>Share</span></p>
+                </div>
             </div>
             <div className="text-message">
                 <input type="text" placeholder='Write your message...'/>
+                <div className="img">
+                <img src={Smile} alt="" />
+                <img src={Link} alt="" />
+                </div>
             </div>
         </div>
     </div>
