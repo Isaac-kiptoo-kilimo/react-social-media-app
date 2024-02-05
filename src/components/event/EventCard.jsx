aeb596295745f9c0adc1c0363876adcc7156283a
unknown <isaackilimok2@gmail.com.com>
2024-02-05 15:21:56 +0300

    feat:fixing errors

--- src/components/event/EventCard.jsx
+++ src/components/event/EventCard.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import './EventCard.scss'
-import EventHeader from './EventHeader'
-import NavIcon from '../NavIcon'
-import EventCalendar from './EventCalendar'
+import React from "react";
+import "./EventCard.scss";
+import EventHeader from "./EventHeader";
+import Event1 from "../../assets/event1.png";
+import Event2 from "../../assets/event2.png";
+import Event3 from "../../assets/event3.png";
+import Event4 from "../../assets/event4.png";
+import Button from "../shared/Button";
+import MembersIcons from "../shared/MembersIcons";
+import EventOne from "../../assets/event-one.png";
+import EventTwo from "../../assets/event-two.png";
+import EventThree from "../../assets/event-three.png";
+import PostImageCard from "../Groups/PostImageCard";
+import EventCalendar from "./EventCalendar";
+import EventVenue from "./EventVenue";
 
 const EventCard = () => {
   return (
@@ -13,22 +23,125 @@ const EventCard = () => {
       <div className="event-sub-titles">
         <div className="events-title-top">
           <h4 className="popular-events">Popular</h4>
-          <h4 >For You</h4>
-          <h4 >Nearest</h4>
-          <h4 >Favorite</h4>
-          <h4 >Registered</h4>
+          <h4>For You</h4>
+          <h4>Nearest</h4>
+          <h4>Favorite</h4>
+          <h4>Registered</h4>
         </div>
-        <EventCalendar/>
-        {/* <div className="btn-events">
-          <NavIcon url={Calendar}/>
-            <h5>November</h5>
-        </div> */}
       </div>
       <div className="events-images">
-       
+        <div className="event-card-contents">
+          <PostImageCard PostImage={Event1} />
+
+          <div className="events-page-icon">
+            <div className="events-page-icon-head">
+              <h3>National Seminar</h3>
+              <p>By Emma Stone</p>
+            </div>
+            <div className="events-bottom">
+              <EventCalendar />
+            </div>
+          </div>
+       <div className="center-events-card">
+       <div className="events-icons">
+         <MembersIcons MbrIcon={EventThree} />
+          <MembersIcons MbrIcon={EventOne} />
+          <MembersIcons MbrIcon={EventTwo} />
+          <div className="events-para">
+            <p>2K</p>
+          </div>
+          
+         </div>
+         <p>+2K are going</p>
+       </div>
+       <EventVenue/>
+          <Button msg="Register" />
+        </div>
+        <div className="event-card-contents">
+          <PostImageCard PostImage={Event2} />
+
+          <div className="events-page-icon">
+            <div className="events-page-icon-head">
+              <h3>National Seminar</h3>
+              <p>By Emma Stone</p>
+            </div>
+            <div className="events-bottom">
+              <EventCalendar />
+            </div>
+          </div>
+       <div className="center-events-card">
+       <div className="events-icons">
+         <MembersIcons MbrIcon={EventThree} />
+          <MembersIcons MbrIcon={EventOne} />
+          <MembersIcons MbrIcon={EventTwo} />
+          <div className="events-para">
+            <p>2K</p>
+          </div>
+          
+         </div>
+         <p>+2K are going</p>
+       </div>
+       <EventVenue/>
+          <Button msg="Register" />
+        </div>
+        <div className="event-card-contents">
+          <PostImageCard PostImage={Event3} />
+
+          <div className="events-page-icon">
+            <div className="events-page-icon-head">
+              <h3>National Seminar</h3>
+              <p>By Emma Stone</p>
+            </div>
+            <div className="events-bottom">
+              <EventCalendar />
+            </div>
+          </div>
+       <div className="center-events-card">
+       <div className="events-icons">
+         <MembersIcons MbrIcon={EventThree} />
+          <MembersIcons MbrIcon={EventOne} />
+          <MembersIcons MbrIcon={EventTwo} />
+          <div className="events-para">
+            <p>2K</p>
+          </div>
+          
+         </div>
+         <p>+2K are going</p>
+       </div>
+       <EventVenue/>
+          <Button msg="Register" />
+        </div>
+        <div className="event-card-contents">
+          <PostImageCard PostImage={Event4} />
+
+          <div className="events-page-icon">
+            <div className="events-page-icon-head">
+              <h3>National Seminar</h3>
+              <p>By Emma Stone</p>
+            </div>
+            <div className="events-bottom">
+              <EventCalendar />
+            </div>
+          </div>
+       <div className="center-events-card">
+       <div className="events-icons">
+         <MembersIcons MbrIcon={EventThree} />
+          <MembersIcons MbrIcon={EventOne} />
+          <MembersIcons MbrIcon={EventTwo} />
+          <div className="events-para">
+            <p>2K</p>
+          </div>
+          
+         </div>
+         <p>+2K are going</p>
+       </div>
+       <EventVenue/>
+          <Button msg="Register" />
+        </div>
+        
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default EventCard
+export default EventCard;
