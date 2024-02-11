8076800fd57ef709b8bdbe01e5bddeea6da23b2a
unknown <isaackilimok2@gmail.com.com>
2024-02-11 21:41:23 +0300

    fix:fixing the events

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -11,22 +11,19 @@ import Notifications from "../components/notifications/Notifications";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Dropdown from "../components/navbar/Dropdown";
-import SideBar from './SideBar'
-import RightBar from './RightBar'
+import SideBar from "./SideBar";
+import RightBar from "./RightBar";
 import RightBarRes from "../components/Responsiveness/RightBarRes";
 import SideBarRes from "../components/Responsiveness/SideBarRes";
 
 const Navbar = () => {
+  const [isNotificationVisible, setNotificationVisibility] = useState(false);
+  const [showProfile, setShowProfile] = useState(false);
+  const navigate = useNavigate();
 
-    const [isNotificationVisible, setNotificationVisibility] = useState(false);
-    const[showProfile, setShowProfile]=useState(false)
-    const navigate = useNavigate();
-
-
-  const [isLogoutVisible , setLogoutVisibility]=useState(false)
-  const [isSidebarVisible , setSidebarVisibility]=useState(false)
-  const [isRightbarVisible , setRightbarVisibility]=useState(false)
-
+  const [isLogoutVisible, setLogoutVisibility] = useState(false);
+  const [isSidebarVisible, setSidebarVisibility] = useState(false);
+  const [isRightbarVisible, setRightbarVisibility] = useState(false);
 
   const handleToggleSidebar = () => {
     setSidebarVisibility(!isSidebarVisible);
@@ -36,78 +33,90 @@ const Navbar = () => {
     setRightbarVisibility(!isRightbarVisible);
   };
 
-
-  const toggleLogout =()=>{
+  const toggleLogout = () => {
     setLogoutVisibility(!isLogoutVisible);
-      console.log("Toggle Logout: ", isLogoutVisible);
+    console.log("Toggle Logout: ", isLogoutVisible);
+  };
 
-  }
+  const handleProfile = () => {
+    setShowProfile(true);
+    navigate("/profile");
+    console.log("navigation");
+  };
 
-    const handleProfile=()=>{
-        setShowProfile(true)
-        navigate("/profile");
-        console.log("navigation");
-    }
-    
-    const toggleNotification = () => {
-        // console.log("Everything is good");
-      setNotificationVisibility(!isNotificationVisible);
-    };
-  
-
+  const toggleNotification = () => {
+    // console.log("Everything is good");
+    setNotificationVisibility(!isNotificationVisible);
+  };
 
   return (
     <>
-    <div className="nav-top">
-    <div className="navbar">
-      <div className="nav-logo">
-        <img src={LogoIcon} alt={LogoIcon} />
-        <div className="resp-menu" onClick={handleToggleSidebar}>
-        <img src={Responsive} alt={Responsive} />
-        <span>Menu</span>
-        </div>
-      </div>
-      <div className="right-nav">
-        <div className="nav-input">
-          <SearchForm />
-        </div>
-        <div className="nav-far-right">
-        <div className="resp-right-menu" onClick={handleToggleRightbar}>
-        <img src={Responsive} alt={Responsive} />
-        </div>
-          <div className="nav-icons">
-            <NavIcon url={Message} />
-            <NavIcon url={ActiveNotification}  onClick={toggleNotification} />
-            <img className="profile" onClick={handleProfile} width={50} src={Profile} alt="profile" />
-            <NavIcon onClick={toggleLogout} url={Chevron} />
+      <div className="nav-top">
+        <div className="navbar">
+          <div className="nav-logo">
+            <img src={LogoIcon} alt={LogoIcon} />
+            <div className="resp-menu" onClick={handleToggleSidebar}>
+              <img src={Responsive} alt={Responsive} />
+              <span>Menu</span>
+            </div>
           </div>
+          <div className="right-nav">
+            <div className="nav-input">
+              <SearchForm />
+            </div>
+            <div className="nav-far-right">
+              <div className="resp-right-menu" onClick={handleToggleRightbar}>
+                <img src={Responsive} alt={Responsive} />
+              </div>
+              <div className="nav-icons">
+                <NavIcon url={Message} />
+                <NavIcon
+                  url={ActiveNotification}
+                  onClick={toggleNotification}
+                />
+                <img
+                  className="profile"
+                  onClick={handleProfile}
+                  width={50}
+                  src={Profile}
+                  alt="profile"
+                />
+                <NavIcon onClick={toggleLogout} url={Chevron} />
+              </div>
+            </div>
+          </div>
+          {isLogoutVisible && (
+            <div className="logout">
+              <Dropdown />
+            </div>
+          )}
         </div>
-        
       </div>
-      {isLogoutVisible && (
-      <div className="logout">
-        <Dropdown/>
-      </div>
-    )
-    }
-    </div>
-    </div>
-    
-    <div className={isSidebarVisible ? "res-sidebar visible" : "res-sidebar"}>
-    <div className="overlay"></div>
-  <div className="sidebar-content">
-    <SideBarRes />
-  </div>
-</div>
 
-   <div className={isRightbarVisible ? "res-rightbar visible" : "res-rightbar"}>
-   <RightBarRes/>
-   </div>
-    {isNotificationVisible && (
-        <div className="notifications-main">
-          <Notifications /> 
+      <div className={isSidebarVisible ? "res-sidebar visible" : "res-sidebar"}>
+        <div className="overlay"></div>
+        <div className="sidebar-content">
+          <SideBarRes />
         </div>
-      )}
+      </div>
+
+      <div
+        className={isRightbarVisible ? "res-rightbar visible" : "res-rightbar"}
+      >
+        <div className="overlay"></div>
+        <RightBarRes />
+      </div>
+     
+
+        {isNotificationVisible && (
+           <div className="not-container">
+           <div className="overlay-not" onClick={toggleNotification}></div>
+          <div className="notifications-main">
+            <Notifications  onClose={toggleNotification}  />
+          </div>
+           </div>
+        )}
+     
     </>
   );
 };
